97d8873520fe1ec6b0f5019071f60f6f04a3ab6a
Zachary Adam-MacEwen <zadammac@kenshosec.com>
2019-08-11 13:07:54 -0300

    "Initial" Commit for prototype dev: - Bring in run.py and app.py from the old dev project - Bring in a blank config file, epc2.ini - Bring in requirements.txt and prepopulate with Flask and Flask_RESTful
    Pausing in development now to begin formal API descriptions.

---
+++ epc2/app.py
@@ -0,0 +1,24 @@
+"""
+This script is a component of the Enumpi Project's back-end controller.
+It is the app-defining component of the Flask-based API/UI service.
+
+Author: Zac Adam-MacEwen (zadammac@kenshosec.com)
+A Kensho Security Labs utility.
+
+Produced under license.
+Full license and documentation to be found at:
+https://github.com/ZAdamMac/Enumpi-C2
+"""
+
+from flask import Blueprint
+from flask_restful import Api
+from resources.helloworld import Hello
+
+__version__ = "prototype"
+
+
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
+
+# New Routes below this line
+api.add_resource(Hello, '/Hello')
